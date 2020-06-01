dd3033ea18fc5b99e4ad1fe5f8cbecda2e533860
Bipin Kumar <bipin.kumar@iontrading.com>
2020-06-01 10:32:30 +0530

    new learnings

--- src/algorithms/sorting/MergeSort.java
+++ src/algorithms/sorting/MergeSort.java
@@ -4,48 +4,46 @@ import java.util.Arrays;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		 int arr[] = {12, 3, 5, 7, 19, 0, -2, 34, 2, 6};
+		int[] array = new int[]{20, 35, 7, -15, 55, 1, 9, -22};
 
-		 System.out.println(Arrays.toString(arr));
-		 sort(arr, 1, arr.length - 2);
-		 System.out.println(Arrays.toString(arr));
+		sort(array, 0, array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
 
-	public static void sort(int[] arr, int low, int high) {
-		if(low < high) {
-			int mid = (low + high) / 2;
-			sort(arr, low, mid);
-			sort(arr, mid + 1, high);
-			merge(arr, low, mid, high);
+	public static void sort(int[] arr, int start, int end){
+		//Base Case: if array partition has only 1 value, return
+		if(end - start < 2){
+			return;
 		}
-	} 
+		int mid = (start + end) / 2;
+		sort(arr, start, mid);
+		sort(arr, mid, end);
+		merge(arr, start, mid, end);
+	}
 
-	private static void merge(int[] arr, int low, int mid, int high) {
-		int[] left = new int[mid - low + 1];
-		int[] right = new int[high - mid];
-		
-		
-		for(int i = 0; i < left.length; i++)
-			left[i] = arr[i + low];
-		
-		for(int j = 0; j < right.length; j++)
-			right[j] = arr[j + mid + 1];
-		int i = 0, j = 0, k = low;
-
-		while(i < left.length && j < right.length) {
-			if(left[i] < right[j]) {
-				arr[k++] = left[i++];
-			} else {
-				arr[k++] = right[j++];
-			}
+	//20, 35, 7, -15, 55, 1, 9, -22
+	private static void merge(int[] arr, int start, int mid, int end) {
+		// Optimisation1: while merging 2 sorted arrays,
+		// if first element of second array is >= last element of first array
+		// then elements of both the array's are already in its right position in main array
+		if (arr[mid-1] <= arr[mid]){
+			return;
 		}
-		
-		while(i < left.length) {
-			arr[k++] = left[i++];
+		int i = start;
+		int j = mid;
+		int tempIndex = 0;
+		int[] tempArr = new int[end - start];
+		// //20, 35, 7, -15, 55, 1, 9, -22
+		while (i < mid && j < end){
+			tempArr[tempIndex++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
 		}
-
-		while(j < right.length) {
-			arr[k++] = right[j++];
-		}		
+		// Optimization2: we need to copy the remaining elements only from left array to the main array
+		// because the remaining element of right array are greater than last element in temp array
+		// and will already in its right position in main array
+		System.arraycopy(arr, i, arr, start + tempIndex, mid -i);
+		System.arraycopy(tempArr, 0, arr, start, tempIndex);
 	}
 }
